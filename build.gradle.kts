f807252119ec501bdaf37939e2f76ba390e385ca
Mike Kobit <mkobit@gmail.com>
2017-03-19 19:29:53 -0500

    The `CIRCLE_REPOSITORY_URL` from Circle CI is an SSH URI and not the HTTPS URI, so just link it for now

--- build.gradle.kts
+++ build.gradle.kts
@@ -37,7 +37,7 @@ buildScan {
     env("CIRCLE_BUILD_URL")?.let { link("Build URL", it) }
     env("CIRCLE_SHA1")?.let { value("Revision", it) }
     env("CIRCLE_COMPARE_URL")?.let { link("Diff", it) }
-    env("CIRCLE_REPOSITORY_URL")?.let { link("Repository", it) }
+    env("CIRCLE_REPOSITORY_URL")?.let { value("Repository", it) }
     env("CIRCLE_PR_NUMBER")?.let { value("Pull Request Number", it) }
   }
 }
