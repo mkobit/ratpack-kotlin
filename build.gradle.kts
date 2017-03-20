f505a8333f24ad3ff5e231e24457851268203396
Mike Kobit <mkobit@gmail.com>
2017-03-19 21:01:57 -0500

    Add in a submodules for `ratpack-test` and `ratpack-guice` and an `afterEvaluate` for checking for `.gitignore` in each subproject

--- build.gradle.kts
+++ build.gradle.kts
@@ -84,3 +84,34 @@ project(":ratpack-core-kotlin") {
     "api"(ratpackModule("core"))
   }
 }
+
+val core = project(":ratpack-core-kotlin")
+
+project(":ratpack-guice-kotlin") {
+  dependencies {
+    "api"(core)
+    "api"(ratpackModule("guice"))
+  }
+}
+
+project(":ratpack-test-kotlin") {
+  dependencies {
+    "api"(core)
+    "api"(ratpackModule("test"))
+  }
+}
+
+afterEvaluate {
+  subprojects.forEach { subproject ->
+    val gitignore: File = subproject.projectDir.resolve(".gitignore")
+
+    if (!gitignore.isFile) {
+      throw GradleException("Subproject ${subproject.name} must have a .gitignore file")
+    }
+    val ignoredBuild = gitignore.readLines(Charsets.UTF_8).filter { it.contentEquals("build/") }
+
+    if (ignoredBuild.isEmpty()) {
+      throw GradleException("Subproject ${subproject.name} does not contain an entry 'build/'")
+    }
+  }
+}
